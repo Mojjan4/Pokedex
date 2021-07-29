2c4e889b071f6fba9555127637f7575c130f4e52
Playground <christopher@playground.tv>
2021-07-29 21:59:26 +0200

    first commit. fetch api pokemon

--- Pokedex/View/Pokedex.swift
+++ Pokedex/View/Pokedex.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct Pokedex: View {
+    private let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
+    @ObservedObject var viewModel = PokemonViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVGrid (columns: gridItems, spacing: 18) {
+                    ForEach(viewModel.pokemon) { pokemon in
+                        PokemonCell(pokemon: pokemon, viewModel: viewModel)
+                    }
+                }
+            }
+            .navigationTitle("Pokedex")
+        }
+        
     }
 }
 
