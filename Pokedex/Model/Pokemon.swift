1b6ea52129ef4b92aa2b51724342917ab083595e
Playground <christopher@playground.tv>
2021-07-29 21:57:19 +0200

    first commit, fetch pokomen api.

---
+++ Pokedex/Model/Pokemon.swift
@@ -0,0 +1,8 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Christopher on 2021-07-29.
+//
+
+import Foundation
---
+++ Pokedex/ViewModel/PokemonViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonViewModel.swift
+//  Pokedex
+//
+//  Created by Christopher on 2021-07-29.
+//
+
+import Foundation
